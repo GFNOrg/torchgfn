93db630a4016334e6d1b6ecc16fc996da9f8df2a
Salem <salemlahlou9@gmail.com>
2022-11-06 17:58:30 -0500

    Add missing inheritance

--- src/gfn/samplers/actions_samplers.py
+++ src/gfn/samplers/actions_samplers.py
@@ -39,7 +39,7 @@ class BackwardActionsSampler(ActionsSampler):
     pass
 
 
-class DiscreteActionsSampler:
+class DiscreteActionsSampler(ActionsSampler):
     """
     For Discrete environments.
     """
