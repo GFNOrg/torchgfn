060788f4ae7fb837fbf9c3af7c5c095bdc6981a6
Joseph Viviano <joseph@viviano.ca>
2023-04-04 23:38:53 -0400

    imports from losses public api, formatting of type checking

--- src/gfn/containers/replay_buffer.py
+++ src/gfn/containers/replay_buffer.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING, Literal
 from gfn.containers.states import States
 from gfn.containers.trajectories import Trajectories
 from gfn.containers.transitions import Transitions
-from gfn.losses.base import (
+from gfn.losses import (
     EdgeDecomposableLoss,
     Loss,
     StateDecomposableLoss,
@@ -14,7 +14,7 @@ from gfn.losses.base import (
 )
 
 if TYPE_CHECKING:
-    from gfn.envs import Env
+   from gfn.envs import Env
 
 
 class ReplayBuffer:
