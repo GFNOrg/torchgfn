112ba819e628b70989897c7c1bec79edcc3e1988
Salem <salemlahlou9@gmail.com>
2023-06-07 14:50:36 -0400

    add assertion when creating actions object

--- src/gfn/actions.py
+++ src/gfn/actions.py
@@ -34,6 +34,10 @@ class Actions(ABC):
             tensor: tensor of actions
         """
         self.tensor = tensor
+        assert len(tensor.shape) > len(self.action_shape), (
+            f"Actions tensor has shape {tensor.shape}, "
+            f"but the action shape is {self.action_shape}."
+        )
         self.batch_shape = tuple(self.tensor.shape)[: -len(self.action_shape)]
 
     @classmethod
