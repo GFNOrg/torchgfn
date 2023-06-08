7c1379e0313a190da46d6e096829ec7857998ab2
Salem <salemlahlou9@gmail.com>
2023-06-08 14:56:44 -0400

    rename Uniform to DiscreteUniform

--- src/gfn/utils/modules.py
+++ src/gfn/utils/modules.py
@@ -108,8 +108,11 @@ class Tabular(nn.Module):
         return outputs
 
 
-class Uniform(nn.Module):
-    """Implements a zero function approximator (a function that always outputs 0).
+class DiscreteUniform(nn.Module):
+    """Implements a uniform distribution over discrete actions.
+
+    It uses a zero function approximator (a function that always outputs 0) to be used as
+    logits by a DiscretePBEstimator.
 
     Attributes:
         output_dim: The size of the output space.
@@ -124,7 +127,7 @@ class Uniform(nn.Module):
         """
         self.output_dim = output_dim
 
-    def __call__(
+    def forward(
         self, preprocessed_states: TT["batch_shape", "input_dim", float]
     ) -> TT["batch_shape", "output_dim", float]:
         out = torch.zeros(*preprocessed_states.shape[:-1], self.output_dim).to(
