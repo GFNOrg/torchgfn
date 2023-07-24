a93e9fe841a7e497d67134379d761786b3cbcb8e
Joseph Viviano <joseph@viviano.ca>
2023-07-24 19:06:28 -0400

    imports fixed

--- src/gfn/parameterizations/__init__.py
+++ src/gfn/parameterizations/__init__.py
@@ -1,4 +1,4 @@
-from .base import PFBasedParametrization, TrajectoryDecomposableLoss
+from .base import PFBasedGFlowNet, TrajectoryDecomposableLoss
 from .detailed_balance import DBParametrization
 from .flow_matching import FMParametrization
 from .sub_trajectory_balance import SubTBParametrization
