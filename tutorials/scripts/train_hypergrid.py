6111a234587d964b433a5347c1da95dbf7e37e3e
Salem <salemlahlou9@gmail.com>
2023-08-01 19:27:58 -0400

    Add replay buffer to train hypergrid script

--- tutorials/scripts/train_hypergrid.py
+++ tutorials/scripts/train_hypergrid.py
@@ -17,6 +17,7 @@ import torch
 import wandb
 from tqdm import tqdm, trange
 
+from gfn.containers import ReplayBuffer
 from gfn.gflownet import (
     DBGFlowNet,
     FMGFlowNet,
@@ -55,6 +56,12 @@ if __name__ == "__main__":  # noqa: C901
         type=int,
         default=16,
         help="Batch size, i.e. number of trajectories to sample per training iteration",
+    )
+    parser.add_argument(
+        "--replay_buffer_size",
+        type=int,
+        default=0,
+        help="If zero, no replay buffer is used. Otherwise, the replay buffer is used.",
     )
 
     parser.add_argument(
@@ -262,6 +269,22 @@ if __name__ == "__main__":  # noqa: C901
 
     assert gflownet is not None, f"No gflownet for loss {args.loss}"
 
+    # Initialize the replay buffer ?
+
+    replay_buffer = None
+    if args.replay_buffer_size > 0:
+        if args.loss in ("TB", "SubTB", "ZVar"):
+            objects_type = "trajectories"
+        elif args.loss == "DB":
+            objects_type = "transitions"
+        elif args.loss == "FM":
+            objects_type = "states"
+        else:
+            raise NotImplementedError(f"Unknown loss: {args.loss}")
+        replay_buffer = ReplayBuffer(
+            env, objects_type=objects_type, capacity=args.replay_buffer_size
+        )
+
     # 3. Create the optimizer
 
     # Policy parameters have their own LR.
@@ -292,9 +315,15 @@ if __name__ == "__main__":  # noqa: C901
     for iteration in trange(n_iterations):
         trajectories = gflownet.sample_trajectories(n_samples=args.batch_size)
         training_samples = gflownet.to_training_samples(trajectories)
+        if replay_buffer is not None:
+            with torch.no_grad():
+                replay_buffer.add(training_samples)
+                training_objects = replay_buffer.sample(n_trajectories=args.batch_size)
+        else:
+            training_objects = training_samples
 
         optimizer.zero_grad()
-        loss = gflownet.loss(training_samples)
+        loss = gflownet.loss(training_objects)
         loss.backward()
         optimizer.step()
 
