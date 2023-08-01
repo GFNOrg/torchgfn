28506670a11eba7244e97f6e80c186109d1a130b
Joseph Viviano <joseph@viviano.ca>
2023-08-01 18:08:56 -0400

    merged trajectories and actions sampler into one

--- src/gfn/samplers/actions_samplers.py
+++ src/gfn/samplers/actions_samplers.py
@@ -1,27 +1,37 @@
-from typing import Tuple
+from typing import Tuple, List, Optional
 
 import torch
 from torchtyping import TensorType as TT
 
 from gfn.actions import Actions
+from gfn.containers import Trajectories
 from gfn.modules import GFNModule
 from gfn.states import States
 
 
 # TODO: Environment should not live inside the estimator and here... needs refactor.
-class ActionsSampler:
-    """`ActionSampler`s are containers for `PolicyEstimator`s.
+class Sampler:
+    """`Sampler is a container for a PolicyEstimator.
+
+    Can be used to either sample individual actions, sample trajectories from $s_0$,
+    or complete a batch of partially-completed trajectories from a given batch states.
 
     Attributes:
         estimator: the submitted PolicyEstimator.
         env: the Environment instance inside the PolicyEstimator.
+        is_backward: if True, samples trajectories of actions backward (a distribution
+            over parents). If True, the estimator must be a ProbabilityDistribution
+            over parents.
     """
 
-    def __init__(self, estimator: GFNModule) -> None:
+    def __init__(self, estimator: GFNModule, is_backward: bool = False) -> None:
         self.estimator = estimator
         self.env = estimator.env
+        self.is_backward = is_backward  # TODO: take directly from estimator.
 
-    def sample(self, states: States) -> Tuple[Actions, TT["batch_shape", torch.float]]:
+    def sample_actions(
+        self, states: States
+    ) -> Tuple[Actions, TT["batch_shape", torch.float]]:
         """Samples actions from the given states.
 
         Args:
@@ -45,152 +55,105 @@ class ActionsSampler:
 
         return self.env.Actions(actions), log_probs
 
+    def sample_trajectories(
+        self,
+        states: Optional[States] = None,
+        n_trajectories: Optional[int] = None,
+    ) -> Trajectories:
+        """Sample trajectories sequentially.
 
-# TODO: Remove rest of file.
+        Args:
+            states: If given, trajectories would start from such states. Otherwise,
+                trajectories are sampled from $s_o$ and n_trajectories must be provided.
+            n_trajectories: If given, a batch of n_trajectories will be sampled all
+                starting from the environment's s_0.
 
-# class ActionsSampler2(ABC):
-#     """
-#     Base class for action sampling methods.
-#     """
+        Returns: A Trajectories object representing the batch of sampled trajectories.
 
-#     @abstractmethod
-#     def sample(self, states: States) -> Tuple[TT["batch_shape", torch.long], TT["batch_shape", torch.long]]:
-#         """
-#         Args:
-#             states (States): A batch of states.
+        Raises:
+            AssertionError: When both states and n_trajectories are specified.
+            AssertionError: When states are not linear.
+        """
+        if states is None:
+            assert (
+                n_trajectories is not None
+            ), "Either states or n_trajectories should be specified"
+            states = self.env.reset(batch_shape=(n_trajectories,))
+        else:
+            assert (
+                len(states.batch_shape) == 1
+            ), "States should be a linear batch of states"
+            n_trajectories = states.batch_shape[0]
 
-#         Returns:
-#             Tuple[Tensor[batch_size], Tensor[batch_size]]: A tuple of tensors containing the log probabilities of the sampled actions, and the sampled actions.
-#         """
-#         pass
+        device = states.tensor.device
 
+        dones = states.is_initial_state if self.is_backward else states.is_sink_state
 
-# class BackwardActionsSampler(ActionsSampler):
-#     """
-#     Base class for backward action sampling methods.
-#     """
+        trajectories_states: List[TT["n_trajectories", "state_shape", torch.float]] = [
+            states.tensor
+        ]
+        trajectories_actions: List[TT["n_trajectories", torch.long]] = []
+        trajectories_logprobs: List[TT["n_trajectories", torch.float]] = []
+        trajectories_dones = torch.zeros(
+            n_trajectories, dtype=torch.long, device=device
+        )
+        trajectories_log_rewards = torch.zeros(
+            n_trajectories, dtype=torch.float, device=device
+        )
 
-#     pass
+        step = 0
 
+        while not all(dones):
+            actions = self.env.Actions.make_dummy_actions(batch_shape=(n_trajectories,))
+            log_probs = torch.full(
+                (n_trajectories,), fill_value=0, dtype=torch.float, device=device
+            )
+            valid_actions, actions_log_probs = self.sample_actions(states[~dones])
+            actions[~dones] = valid_actions
+            log_probs[~dones] = actions_log_probs
+            trajectories_actions += [actions]
+            trajectories_logprobs += [log_probs]
 
-# class DiscreteActionsSampler(ActionsSampler):
-#     """
-#     For Discrete environments.
-#     """
+            if self.is_backward:
+                new_states = self.env.backward_step(states, actions)
+            else:
+                new_states = self.env.step(states, actions)
+            sink_states_mask = new_states.is_sink_state
 
-#     def __init__(
-#         self,
-#         estimator: DiscretePFEstimator | LogEdgeFlowEstimator,
-#         temperature: float = 1.0,
-#         sf_bias: float = 0.0,
-#         epsilon: float = 0.0,
-#     ) -> None:
-#         """Implements a method that samples actions from any given batch of states.
+            step += 1
 
-#         Args:
-#             temperature (float, optional): scalar to divide the logits by before softmax. Defaults to 1.0.
-#             sf_bias (float, optional): scalar to subtract from the exit action logit before dividing by temperature. Defaults to 0.0.
-#             epsilon (float, optional): with probability epsilon, a random action is chosen. Defaults to 0.0.
-#         """
-#         self.estimator = estimator
-#         self.temperature = temperature
-#         self.sf_bias = sf_bias
-#         self.epsilon = epsilon
+            new_dones = (
+                new_states.is_initial_state if self.is_backward else sink_states_mask
+            ) & ~dones
+            trajectories_dones[new_dones & ~dones] = step
+            try:
+                trajectories_log_rewards[new_dones & ~dones] = self.env.log_reward(
+                    states[new_dones & ~dones]
+                )
+            except NotImplementedError:
+                # print(states[new_dones & ~dones])
+                # print(torch.log(self.env.reward(states[new_dones & ~dones])))
+                trajectories_log_rewards[new_dones & ~dones] = torch.log(
+                    self.env.reward(states[new_dones & ~dones])
+                )
+            states = new_states
+            dones = dones | new_dones
 
-#     def get_raw_logits(self, states: States) -> TensorBatchActions:
-#         """
-#         This is before illegal actions are masked out and the exit action is biased.
-#         Should be used for Discrete action spaces only.
+            trajectories_states += [states.tensor]
 
-#         Returns:
-#             A 2D tensor of shape (batch_size, n_actions) containing the logits for each action in each state in the batch.
-#         """
-#         logits = self.estimator(states)
-#         return logits
+        trajectories_states = torch.stack(trajectories_states, dim=0)
+        trajectories_states = self.env.States(tensor=trajectories_states)
+        trajectories_actions = self.env.Actions.stack(trajectories_actions)
+        trajectories_logprobs = torch.stack(trajectories_logprobs, dim=0)
 
-#     def get_logits(self, states: States) -> TT["batch_shape", "action_shape"]:
-#         """Transforms the raw logits by masking illegal actions.
+        trajectories = Trajectories(
+            env=self.env,
+            states=trajectories_states,
+            actions=trajectories_actions,
+            when_is_done=trajectories_dones,
+            is_backward=self.is_backward,
+            log_rewards=trajectories_log_rewards,
+            log_probs=trajectories_logprobs,
+        )
 
-#         Raises:
-#             ValueError: if one of the resulting logits is NaN.
-
-#         Returns:
-#             A 2D tensor of shape (batch_size, n_actions) containing the transformed logits.
-#         """
-#         logits = self.get_raw_logits(states)
-
-#         if torch.any(torch.all(torch.isnan(logits), 1)):
-#             raise ValueError("NaNs in estimator")
-#         states.forward_masks, _ = correct_cast(
-#             states.forward_masks, states.backward_masks
-#         )
-#         logits[~states.forward_masks] = -float("inf")
-#         return logits
-
-#     def get_probs(
-#         self,
-#         states: States,
-#     ) -> TT["batch_shape", "action_shape"]:
-#         """
-#         Returns:
-#             The probabilities of each action in each state in the batch.
-#         """
-#         logits = self.get_logits(states)
-#         logits[..., -1] -= self.sf_bias
-#         probs = torch.softmax(logits / self.temperature, dim=-1)
-#         return probs
-
-#     def sample(self, states: States) -> Tuple[TT["batch_shape", torch.long], TT["batch_shape", torch.long]]:
-#         probs = self.get_probs(states)
-#         states.forward_masks, _ = correct_cast(
-#             states.forward_masks, states.backward_masks
-#         )
-#         if self.epsilon > 0:
-#             uniform_dist = (
-#                 states.forward_masks.float()
-#                 / states.forward_masks.sum(dim=-1, keepdim=True).float()
-#             )
-#             probs = (1 - self.epsilon) * probs + self.epsilon * uniform_dist
-#         dist = Categorical(probs=probs)
-#         with torch.no_grad():
-#             actions = dist.sample()
-#         actions_log_probs = dist.log_prob(actions)
-
-#         return actions_log_probs, actions
-
-
-# class BackwardDiscreteActionsSampler(DiscreteActionsSampler, BackwardActionsSampler):
-#     """
-#     For sampling backward actions in discrete environments.
-#     """
-
-#     def __init__(
-#         self,
-#         estimator: DiscretePBEstimator,
-#         temperature: float = 1.0,
-#         epsilon: float = 0.0,
-#     ) -> None:
-#         """s_f is not biased in the backward sampler."""
-#         super().__init__(
-#             estimator, temperature=temperature, sf_bias=0.0, epsilon=epsilon
-#         )
-
-#     def get_logits(self, states: States) -> TT["batch_shape", "action_shape"]:
-#         logits = self.get_raw_logits(states)
-#         if torch.any(torch.all(torch.isnan(logits), 1)):
-#             raise ValueError("NaNs in estimator")
-#         _, states.backward_masks = correct_cast(
-#             states.forward_masks, states.backward_masks
-#         )
-#         logits[~states.backward_masks] = -float("inf")
-#         return logits
-
-#     def get_probs(self, states: States) -> TT["batch_shape", "action_shape"]:
-#         logits = self.get_logits(states)
-#         probs = torch.softmax(logits / self.temperature, dim=-1)
-#         # The following line is hack that works: when probs are nan, it means
-#         # that the state is already done (usually during backward sampling).
-#         # In which case, any action can be passed to the backward_step function
-#         # making the state stay at s_0
-#         probs = probs.nan_to_num(nan=1.0 / probs.shape[-1])
-#         return probs
+        return trajectories
