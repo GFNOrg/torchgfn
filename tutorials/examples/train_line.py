e88e1bb59c5820a3f9d43e838a83cbb844491d72
Joseph <joseph@viviano.ca>
2023-11-22 11:47:28 -0500

    v1 of the line tutorial

--- tutorials/examples/train_line.py
+++ tutorials/examples/train_line.py
@@ -1,44 +1,39 @@
-from tqdm import tqdm, trange
-from typing import ClassVar, Literal, Tuple  # ClassVar, Literal, Tuple, cast
-import math
+import random
+from typing import ClassVar, Literal, Tuple
+
 import matplotlib.pyplot as plt
 import numpy as np
-import random
-import scipy.stats as stats
-
-from torchtyping import TensorType as TT
 import torch
-import torch.nn as nn
-from torch.distributions import Distribution, Normal, Beta
+from torch.distributions import Distribution, Normal  # TODO: extend to Beta
+from torchtyping import TensorType as TT
+from tqdm import trange
 
 from gfn.actions import Actions
 from gfn.env import Env
-from gfn.gflownet import TBGFlowNet, SubTBGFlowNet
+from gfn.gflownet import TBGFlowNet  # TODO: Extend to SubTBGFlowNet
 from gfn.modules import GFNModule
 from gfn.states import States
 from gfn.utils import NeuralNet
 
+
 class Line(Env):
-    """Line environment."""
+    """Mixture of Gaussians Line environment."""
 
     def __init__(
         self,
         mus: list,
-        variances: list,
+        sigmas: list,
         init_value: float,
         n_sd: float = 4.5,
         n_steps_per_trajectory: int = 5,
         device_str: Literal["cpu", "cuda"] = "cpu",
     ):
-        assert len(mus) == len(variances)
+        assert len(mus) == len(sigmas)
         self.mus = torch.tensor(mus)
-        self.sigmas = torch.tensor([math.sqrt(v) for v in variances])
-        self.variances = torch.tensor(variances)
+        self.sigmas = torch.tensor(sigmas)
         self.n_sd = n_sd
         self.n_steps_per_trajectory = n_steps_per_trajectory
-        self.mixture = [
-            Normal(m, s) for m, s in zip(self.mus, self.sigmas)
-        ]
+        self.mixture = [Normal(m, s) for m, s in zip(self.mus, self.sigmas)]
 
         self.init_value = init_value  # Used in s0.
         self.lb = min(self.mus) - self.n_sd * max(self.sigmas)  # Convienience only.
@@ -63,43 +58,48 @@ class Line(Env):
 
         class LineActions(Actions):
             action_shape: ClassVar[Tuple[int, ...]] = (1,)  # Does not include counter!
-            dummy_action: ClassVar[TT[2]] = torch.tensor([float("inf")], device=env.device)
-            exit_action: ClassVar[TT[2]] = torch.tensor([-float("inf")], device=env.device)
+            dummy_action: ClassVar[TT[2]] = torch.tensor(
+                [float("inf")], device=env.device
+            )
+            exit_action: ClassVar[TT[2]] = torch.tensor(
+                [-float("inf")], device=env.device
+            )
 
         return LineActions
 
-    def maskless_step(self, states: States, actions: Actions) -> TT["batch_shape", 2, torch.float]:
-        states.tensor[..., 0] = states.tensor[..., 0] + actions.tensor.squeeze(-1)  # x position.
+    def maskless_step(
+        self, states: States, actions: Actions
+    ) -> TT["batch_shape", 2, torch.float]:
+        states.tensor[..., 0] = states.tensor[..., 0] + actions.tensor.squeeze(
+            -1
+        )  # x position.
         states.tensor[..., 1] = states.tensor[..., 1] + 1  # Step counter.
         return states.tensor
 
-    def maskless_backward_step(self, states: States, actions: Actions) -> TT["batch_shape", 2, torch.float]:
-        states.tensor[..., 0] = states.tensor[..., 0] - actions.tensor.squeeze(-1)  # x position.
+    def maskless_backward_step(
+        self, states: States, actions: Actions
+    ) -> TT["batch_shape", 2, torch.float]:
+        states.tensor[..., 0] = states.tensor[..., 0] - actions.tensor.squeeze(
+            -1
+        )  # x position.
         states.tensor[..., 1] = states.tensor[..., 1] - 1  # Step counter.
         return states.tensor
 
-    def is_action_valid(self, states: States, actions: Actions, backward: bool = False) -> bool:
+    def is_action_valid(
+        self, states: States, actions: Actions, backward: bool = False
+    ) -> bool:
         # Can't take a backward step at the beginning of a trajectory.
         if torch.any(states[~actions.is_exit].is_initial_state) and backward:
             return False
 
         return True
 
-    # def reward(self, final_states: States) -> TT["batch_shape", torch.float]:
-    #     s = final_states.tensor[..., 0]
-    #     if s.nelement() == 0:
-    #         return torch.zeros(final_states.batch_shape)
-
-    #     rewards = torch.empty(final_states.batch_shape)
-    #     for i, m in enumerate(self.mixture):
-    #         rewards = rewards + torch.exp(m.log_prob(s))
-
-    #     return rewards
-
     def log_reward(self, final_states: States) -> TT["batch_shape", torch.float]:
         s = final_states.tensor[..., 0]
-        if s.nelement() == 0:
-            return torch.zeros(final_states.batch_shape)
+        # return torch.logsumexp(torch.stack([m.log_prob(s) for m in self.mixture], 0), 0)
+
+        # if s.nelement() == 0:
+        #     return torch.zeros(final_states.batch_shape)
 
         log_rewards = torch.empty((len(self.mixture),) + final_states.batch_shape)
         for i, m in enumerate(self.mixture):
@@ -123,10 +123,8 @@ def render(env, validation_samples=None):
 
     # Get the rewards from our environment.
     r = env.States(
-        torch.tensor(
-            np.stack((x, torch.ones(len(x))), 1)  # Add dummy counter.
-            )
-        )
+        torch.tensor(np.stack((x, torch.ones(len(x))), 1))  # Add dummy state counter.
+    )
     d = torch.exp(env.log_reward(r))  # Plots the reward, not the log reward.
 
     fig, ax1 = plt.subplots()
@@ -134,7 +132,7 @@ def render(env, validation_samples=None):
     if not isinstance(validation_samples, type(None)):
         ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis.
         ax2.hist(
-            validation_samples.tensor[:,0].cpu().numpy(),
+            validation_samples.tensor[:, 0].cpu().numpy(),
             bins=100,
             density=False,
             alpha=0.5,
@@ -150,16 +148,16 @@ def render(env, validation_samples=None):
         ax1.axvline(mu, color="grey", linestyle="--")
 
     # S0
-    ax1.plot([env.init_value], [0], 'ro')
+    ax1.plot([env.init_value], [0], "ro")
     ax1.text(env.init_value + 0.1, 0.01, "$S_0$", rotation=45)
 
     # Means
     for i, mu in enumerate(env.mus):
         idx = abs(x - mu.numpy()) == min(abs(x - mu.numpy()))
-        ax1.plot([x[idx]], [d[idx]], 'bo')
+        ax1.plot([x[idx]], [d[idx]], "bo")
         ax1.text(x[idx] + 0.1, d[idx], "Mode {}".format(i + 1), rotation=0)
 
-    ax1.spines[['right', 'top']].set_visible(False)
+    ax1.spines[["right", "top"]].set_visible(False)
     ax1.set_ylabel("Reward Value")
     ax1.set_xlabel("X Position")
     ax1.set_title("Line Environment")
@@ -171,6 +169,7 @@ class ScaledGaussianWithOptionalExit(Distribution):
     """Extends the Beta distribution by considering the step counter. When sampling,
     the step counter can be used to ensure the `exit_action` [inf, inf] is sampled.
     """
+
     def __init__(
         self,
         states: TT["n_states", 2],  # Tensor of [x position, step counter].
@@ -206,30 +205,31 @@ class ScaledGaussianWithOptionalExit(Distribution):
         actions_to_eval = torch.full_like(sampled_actions, 0)  # Used to remove infs.
 
         # TODO: Continous Timestamp Environmemt Subclass.
-        if self.backward:
-            exit_idx = self.states[..., 1].tensor == 1  # This is the s1->s0 action.
-        else:  # Forward: handle exit actions.
-            exit_idx = torch.all(sampled_actions == -float("inf"), 1)  # This is the sn->sf action.
+        if self.backward:  # Backward: handle the s1->s0 action (always p=1).
+            exit_idx = self.states[..., 1].tensor == 1
+        else:  # Forward: handle exit actions: sn->sf.
+            exit_idx = torch.all(sampled_actions == -float("inf"), 1)
 
         actions_to_eval[~exit_idx] = sampled_actions[~exit_idx]
         if sum(~exit_idx) > 0:
-            logprobs[~exit_idx] = self.dist.log_prob(actions_to_eval)[~exit_idx]  # TODO: inefficient!
+            logprobs[~exit_idx] = self.dist.log_prob(actions_to_eval)[~exit_idx]
 
         return logprobs.squeeze(-1)
 
 
 class GaussianStepNeuralNet(NeuralNet):
     """A deep neural network for the forward and backward policy."""
+
     def __init__(
-            self,
-            hidden_dim: int,
-            n_hidden_layers: int,
-            policy_std_min: float = 0.1,
-            policy_std_max: float = 1,
-        ):
+        self,
+        hidden_dim: int,
+        n_hidden_layers: int,
+        policy_std_min: float = 0.1,
+        policy_std_max: float = 1,
+    ):
         """Instantiates the neural network for the forward policy."""
         assert policy_std_min > 0
-        assert policy_std_min <  policy_std_max
+        assert policy_std_min < policy_std_max
         self.policy_std_min = policy_std_min
         self.policy_std_max = policy_std_max
         self.input_dim = 2  # [x_pos, counter].
@@ -243,20 +243,27 @@ class GaussianStepNeuralNet(NeuralNet):
             activation_fn="elu",
         )
 
-    def forward(self, preprocessed_states: TT["batch_shape", 2, float]) -> TT["batch_shape", "3"]:
+    def forward(
+        self, preprocessed_states: TT["batch_shape", 2, float]
+    ) -> TT["batch_shape", "3"]:
+        """Calculate the gaussian parameters, applying the bound to sigma."""
         assert preprocessed_states.ndim == 2
         out = super().forward(preprocessed_states)  # [..., 2]: represents mean & std.
-        minmax_norm = (self.policy_std_max - self.policy_std_min)
-        out[..., 1] = torch.sigmoid(out[..., 1]) * minmax_norm + self.policy_std_min  # Scales / Variances.
+        minmax_norm = self.policy_std_max - self.policy_std_min
+        out[..., 1] = (
+            torch.sigmoid(out[..., 1]) * minmax_norm + self.policy_std_min
+        )  # Scales / Variances.
 
         return out
 
 
 class StepEstimator(GFNModule):
-    """Estimator for P_F and P_B of the Line environment."""
+    """Estimator for PF and PB of the Line environment."""
+
     def __init__(self, env: Line, module: torch.nn.Module, backward: bool):
-        super().__init__(module, is_backward = backward)  # TODO: We should always only use backward or forward to avoid negation.
+        super().__init__(module, is_backward=backward)
         self.backward = backward
+        self.n_steps_per_trajectory = env.n_steps_per_trajectory
 
     def expected_output_dim(self) -> int:
         return 2  # [locs, scales].
@@ -265,9 +272,8 @@ class StepEstimator(GFNModule):
         self,
         states: States,
         module_output: TT["batch_shape", "output_dim", float],
-        scale_factor = 0,  # policy_kwarg.
+        scale_factor=0,  # policy_kwarg.
     ) -> Distribution:
-        # First, we verify that the batch shape of states is 1
         assert len(states.batch_shape) == 1
         assert module_output.shape == states.batch_shape + (2,)  # [locs, scales].
         locs, scales = torch.split(module_output, [1, 1], dim=-1)
@@ -277,19 +283,9 @@ class StepEstimator(GFNModule):
             locs,
             scales + scale_factor,  # Increase this value to induce exploration.
             backward=self.backward,
-            n_steps=env.n_steps_per_trajectory,
+            n_steps=self.n_steps_per_trajectory,
         )
 
-
-# def get_scheduler(optim, n_iter, n_steps_scheduler=1500, scheduler_gamma=0.5):
-#     return torch.optim.lr_scheduler.MultiStepLR(
-#         optim,
-#         milestones=[
-#             i * n_steps_scheduler
-#             for i in range(1, 1 + int(n_iter / n_steps_scheduler))
-#         ],
-#         gamma=scheduler_gamma,
-#     )
 
 def fix_seed(seed):
     """Reproducibility."""
@@ -301,28 +297,25 @@ def fix_seed(seed):
 
 
 def train(
-        gflownet,
-        seed=4444,
-        n_trajectories=3e6,
-        batch_size=128,
-        lr_base=1e-3,
-        gradient_clip_value=10,
-        n_logz_resets=0,
-        exploration_var_starting_val=2,
-    ):
+    gflownet,
+    env,
+    seed=4444,
+    n_trajectories=3e6,
+    batch_size=128,
+    lr_base=1e-3,
+    gradient_clip_value=5,
+    exploration_var_starting_val=2,
+):
+    """Trains a GFlowNet on the Line Environment."""
     fix_seed(seed)
-    device_str = "cuda" if torch.cuda.is_available() else "cpu"
     n_iterations = int(n_trajectories // batch_size)
-    logz_reset_interval = 50
-    logz_reset_count = 0
-
 
     # TODO: Add in the uniform pb demo?
-    #uniform_pb = False
+    # uniform_pb = False
     #
-    #if uniform_pb:
+    # if uniform_pb:
     #    pb_module = BoxPBUniform()
-    #else:
+    # else:
     #    pb_module = BoxPBNeuralNet(hidden_dim, n_hidden_layers, n_components)
 
     # 3. Create the optimizer and scheduler.
@@ -330,39 +323,12 @@ def train(
     lr_logZ = lr_base * 100
     optimizer.add_param_group({"params": gflownet.logz_parameters(), "lr": lr_logZ})
 
-    # TODO:
-    # if not uniform_pb:
-    #     optimizer.add_param_group({"params": pb_module.parameters(), "lr": lr_base})
-    # optimizer.add_param_group({"params": logFmodule.parameters(), "lr": lr_logF})
-
-    # scheduler = get_scheduler(
-    #     optimizer,
-    #     n_iterations,
-    #     n_steps_scheduler=int(n_iterations / 2),
-    #     scheduler_gamma=0.5,
-    # )
-
-    # TODO:
-    # 4. Sample from the true reward distribution, and fit a KDE to the samples.
-    # n_val_samples = 1000
-    # samples_from_reward = sample_from_reward(env, n_samples=n_val_samples)
-    # true_kde = KernelDensity(kernel="exponential", bandwidth=0.1).fit(
-    #     samples_from_reward
-    # )
-
-    # Training loop!
-    # validation_interval = 1e4
+    # Training loop.
     states_visited = 0
-    jsd = float("inf")
     tbar = trange(n_iterations, desc="Training iter")
     scale_schedule = np.linspace(exploration_var_starting_val, 0, n_iterations)
 
     for iteration in tbar:
-
-        # if logz_reset_count < n_logz_resets and iteration % logz_reset_interval == 0:
-        #     gflownet.logZ = torch.nn.init.constant_(gflownet.logZ, 0)
-        #     print("resetting logz")
-        #     logz_reset_count += 1
 
         # Off Policy Sampling.
         trajectories, estimator_outputs = gflownet.sample_trajectories(
@@ -375,59 +341,48 @@ def train(
         optimizer.zero_grad()
         loss = gflownet.loss(env, training_samples, estimator_outputs=estimator_outputs)
         loss.backward()
-        # print("{} / {} ".format(
-        #     dict(gflownet.named_parameters())["pf.module.last_layer.bias"],
-        #     dict(gflownet.named_parameters())["pb.module.last_layer.bias"],
-        #     )
-        # )
 
-        # # LESSON: Clipping
-        # for p in gflownet.parameters():
-        #     if p.ndim > 0 and p.grad is not None:  # We do not clip logZ grad.
-        #         p.grad.data.clamp_(-gradient_clip_value, gradient_clip_value).nan_to_num_(0.0)
+        # Gradient Clipping.
+        for p in gflownet.parameters():
+            if p.ndim > 0 and p.grad is not None:  # We do not clip logZ grad.
+                p.grad.data.clamp_(
+                    -gradient_clip_value, gradient_clip_value
+                ).nan_to_num_(0.0)
 
         optimizer.step()
-        # scheduler.step()
         states_visited += len(trajectories)
-        assert gflownet.logz_parameters()[0].item() is not None
 
-        #to_log = {"loss": loss.item(), "states_visited": states_visited}
-        # logZ_info = ""
-        #to_log.update({"logZdiff": env.log_partition - logZ.item()})
-        # logZ_info = f"logZ: {:.2f}, "
-        #print(logFmodule.logZ_value)
-        #print(gflownet.logZ)
-
-        tbar.set_description("Training iter {}: (states visited={}, loss={:.3f}, estimated logZ={:.3f}, true logZ={:.3f}, JSD={}, LR={})".format(
-            iteration,
-            states_visited,
-            loss.item(),
-            gflownet.logz_parameters()[0].item(),  # Assumes only one estimate of logZ.
-            env.log_partition,
-            jsd,
-            optimizer.param_groups[0]['lr'],
+        tbar.set_description(
+            "Training iter {}: (states visited={}, loss={:.3f}, estimated logZ={:.3f}, true logZ={:.3f})".format(
+                iteration,
+                states_visited,
+                loss.item(),
+                gflownet.logz_parameters()[
+                    0
+                ].item(),  # Assumes only one estimate of logZ.
+                env.log_partition,
             )
         )
-        # if iteration % validation_interval == 0:
-        #     validation_samples = gflownet.sample_terminating_states(env, n_val_samples)
-        #     kde = KernelDensity(kernel="exponential", bandwidth=0.1).fit(
-        #         validation_samples.tensor.detach().cpu().numpy()
-        #     )
-        #     jsd = estimate_jsd(kde, true_kde)
-        #     to_log.update({"JSD": jsd})
 
-    return gflownet, jsd
+    return gflownet
 
 
 if __name__ == "__main__":
 
-    env = Line(mus=[2, 5], variances=[0.2, 0.2], init_value=0, n_sd=4.5, n_steps_per_trajectory=5)
-    # Forward and backward policy estimators. We pass the lower bound from the env here.
+    environment = Line(
+        mus=[2, 5],
+        sigmas=[0.5, 0.5],
+        init_value=0,
+        n_sd=4.5,
+        n_steps_per_trajectory=5,
+    )
+
+    # Hyperparameters.
     hid_dim = 64
     n_hidden_layers = 2
-    policy_std_min = 0.1
-    policy_std_max = 1
-    exploration_var_starting_val = 2
+    policy_std_min = 0.1  # Lower bound of sigma that can be predicted by policy.
+    policy_std_max = 1  # Upper bound of sigma that can be predicted by policy.
+    exploration_var_starting_val = 2  # Used for off-policy training.
 
     pf_module = GaussianStepNeuralNet(
         hidden_dim=hid_dim,
@@ -435,11 +390,7 @@ if __name__ == "__main__":
         policy_std_min=policy_std_min,
         policy_std_max=policy_std_max,
     )
-    pf_estimator = StepEstimator(
-        env,
-        pf_module,
-        backward=False,
-    )
+    pf = StepEstimator(environment, pf_module, backward=False)
 
     pb_module = GaussianStepNeuralNet(
         hidden_dim=hid_dim,
@@ -447,27 +398,17 @@ if __name__ == "__main__":
         policy_std_min=policy_std_min,
         policy_std_max=policy_std_max,
     )
-    pb_estimator = StepEstimator(
-        env,
-        pb_module,
-        backward=True,
-    )
+    pb = StepEstimator(environment, pb_module, backward=True)
+    gflownet = TBGFlowNet(pf=pf, pb=pb, on_policy=False, init_logZ=0.0)
 
-    gflownet = TBGFlowNet(
-        pf=pf_estimator,
-        pb=pb_estimator,
-        on_policy=False,
-        init_logZ=0.0,
-    )
-
-    # Magic hyperparameters: lr_base=4e-2, n_trajectories=3e6, batch_size=2048
-    gflownet, jsd = train(
+    gflownet = train(
         gflownet,
+        environment,
         lr_base=1e-3,
         n_trajectories=1.28e6,
-        batch_size=1,  # 256
-        exploration_var_starting_val=exploration_var_starting_val
-    )  # I started training this with 1e-3 and then reduced it.
+        batch_size=256,
+        exploration_var_starting_val=exploration_var_starting_val,
+    )
 
-    validation_samples = gflownet.sample_terminating_states(env, 10000)
-    render(env, validation_samples=validation_samples)
+    validation_samples = gflownet.sample_terminating_states(environment, 10000)
+    render(environment, validation_samples=validation_samples)
