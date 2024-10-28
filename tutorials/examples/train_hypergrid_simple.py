b6e042c5736e5f0be71818cda7faf3a1add569e4
hyeok9855 <hyeok9855@gmail.com>
2024-10-28 13:31:30 +0900

    use argparse in

--- tutorials/examples/train_hypergrid_simple.py
+++ tutorials/examples/train_hypergrid_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import argparse
+
 import torch
 from tqdm import tqdm
 
@@ -6,57 +8,94 @@ from gfn.gflownet import TBGFlowNet
 from gfn.gym import HyperGrid
 from gfn.modules import DiscretePolicyEstimator
 from gfn.samplers import Sampler
+from gfn.utils.common import set_seed
 from gfn.utils.modules import MLP
 
-torch.manual_seed(0)
 
-# Setup the Environment.
-env = HyperGrid(
-    ndim=4,
-    height=16,
-    device_str="cuda" if torch.cuda.is_available() else "cpu",
-)
+def main(args):
+    set_seed(args.seed)
+    device_str = "cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu"
 
-# Build the GFlowNet.
-module_PF = MLP(
-    input_dim=env.preprocessor.output_dim,
-    output_dim=env.n_actions,
-)
-module_PB = MLP(
-    input_dim=env.preprocessor.output_dim,
-    output_dim=env.n_actions - 1,
-    torso=module_PF.torso,
-)
-pf_estimator = DiscretePolicyEstimator(
-    module_PF, env.n_actions, is_backward=False, preprocessor=env.preprocessor
-)
-pb_estimator = DiscretePolicyEstimator(
-    module_PB, env.n_actions, is_backward=True, preprocessor=env.preprocessor
-)
-gflownet = TBGFlowNet(pf=pf_estimator, pb=pb_estimator, logZ=0.0)
+    # Setup the Environment.
+    env = HyperGrid(ndim=args.ndim, height=args.height, device_str=device_str)
 
-# Feed pf to the sampler.
-sampler = Sampler(estimator=pf_estimator)
-
-# Move the gflownet to the GPU.
-if torch.cuda.is_available():
-    gflownet = gflownet.to("cuda")
-
-# Policy parameters have their own LR. Log Z gets dedicated learning rate
-# (typically higher).
-optimizer = torch.optim.Adam(gflownet.pf_pb_parameters(), lr=1e-3)
-optimizer.add_param_group({"params": gflownet.logz_parameters(), "lr": 1e-1})
-
-for i in (pbar := tqdm(range(1000))):
-    trajectories = sampler.sample_trajectories(
-        env,
-        n_trajectories=16,
-        save_logprobs=False,
-        save_estimator_outputs=True,
-        epsilon=0.1,
+    # Build the GFlowNet.
+    module_PF = MLP(
+        input_dim=env.preprocessor.output_dim,
+        output_dim=env.n_actions,
     )
-    optimizer.zero_grad()
-    loss = gflownet.loss(env, trajectories)
-    loss.backward()
-    optimizer.step()
-    pbar.set_postfix({"loss": loss.item()})
+    module_PB = MLP(
+        input_dim=env.preprocessor.output_dim,
+        output_dim=env.n_actions - 1,
+        torso=module_PF.torso,
+    )
+    pf_estimator = DiscretePolicyEstimator(
+        module_PF, env.n_actions, is_backward=False, preprocessor=env.preprocessor
+    )
+    pb_estimator = DiscretePolicyEstimator(
+        module_PB, env.n_actions, is_backward=True, preprocessor=env.preprocessor
+    )
+    gflownet = TBGFlowNet(pf=pf_estimator, pb=pb_estimator, logZ=0.0)
+
+    # Feed pf to the sampler.
+    sampler = Sampler(estimator=pf_estimator)
+
+    # Move the gflownet to the GPU.
+    if torch.cuda.is_available():
+        gflownet = gflownet.to("cuda")
+
+    # Policy parameters have their own LR. Log Z gets dedicated learning rate
+    # (typically higher).
+    optimizer = torch.optim.Adam(gflownet.pf_pb_parameters(), lr=args.lr)
+    optimizer.add_param_group(
+        {"params": gflownet.logz_parameters(), "lr": args.lr_logz}
+    )
+
+    for i in (pbar := tqdm(range(args.n_iterations))):
+        trajectories = sampler.sample_trajectories(
+            env,
+            n_trajectories=args.batch_size,
+            save_logprobs=False,
+            save_estimator_outputs=True,
+            epsilon=args.epsilon,
+        )
+        optimizer.zero_grad()
+        loss = gflownet.loss(env, trajectories)
+        loss.backward()
+        optimizer.step()
+        pbar.set_postfix({"loss": loss.item()})
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--no_cuda", action="store_true", help="Prevent CUDA usage")
+    parser.add_argument(
+        "--ndim", type=int, default=4, help="Number of dimensions in the environment"
+    )
+    parser.add_argument(
+        "--height", type=int, default=16, help="Height of the environment"
+    )
+    parser.add_argument("--seed", type=int, default=0, help="Random seed")
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=1e-3,
+        help="Learning rate for the estimators' modules",
+    )
+    parser.add_argument(
+        "--lr_logz",
+        type=float,
+        default=1e-1,
+        help="Learning rate for the logZ parameter",
+    )
+    parser.add_argument(
+        "--n_iterations", type=int, default=1000, help="Number of iterations"
+    )
+    parser.add_argument("--batch_size", type=int, default=16, help="Batch size")
+    parser.add_argument(
+        "--epsilon", type=float, default=0.1, help="Epsilon for the sampler"
+    )
+
+    args = parser.parse_args()
+
+    main(args)
