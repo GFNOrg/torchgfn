63dfeabb4bae48e310096ff4b2d59a3bb88e6b03
alexandrelarouche <quoding@protonmail.ch>
2025-05-30 14:31:44 -0400

    Remove ensure_same_device from Env

--- src/gfn/env.py
+++ src/gfn/env.py
@@ -51,7 +51,6 @@ class Env(ABC):
         )
 
         assert self.s0.shape == self.sf.shape == state_shape
-        ensure_same_device(self.s0.device, self.sf.device)
 
         self.state_shape = state_shape
         self.action_shape = action_shape
