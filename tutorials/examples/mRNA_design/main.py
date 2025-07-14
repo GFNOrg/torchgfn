420789c910f994a1fb8c24745055bd5b609afeac
ayalaajil <ayalaajil132002@gmail.com>
2025-07-14 11:47:51 +0000

    Address review comments

--- tutorials/examples/mRNA_design/main.py
+++ tutorials/examples/mRNA_design/main.py
@@ -1,21 +1,21 @@
 import argparse
 import logging
-import time
 from datetime import datetime
 
+import numpy as np
 import torch
 from env import CodonDesignEnv
 from evaluate import evaluate
-from plots import plot_training_curves
 from preprocessor import CodonSequencePreprocessor
-from train import train
-from utils import load_config
+from tqdm import tqdm
+from utils import compute_reward, load_config, plot_training_curves
 
 from gfn.gflownet import TBGFlowNet
 from gfn.modules import DiscretePolicyEstimator
 from gfn.samplers import Sampler
 from gfn.utils.modules import MLP
 
+# Logging setup
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
@@ -26,11 +26,9 @@ logging.basicConfig(
 def main(args):
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    logging.info(f"Using device: {device}")
+    config = load_config(args.config_path)
 
-    start_time = time.time()
-
-    # 1. Create the environment.
+    # 1. Create the environment and preprocessor
     env = CodonDesignEnv(protein_seq=config.protein_seq, device=device)
     preprocessor = CodonSequencePreprocessor(
         env.seq_length, embedding_dim=args.embedding_dim, device=device
@@ -60,52 +58,64 @@ def main(args):
     )
 
     # 2. Create the gflownet.
-    gflownet = TBGFlowNet(pf=pf_estimator, pb=pb_estimator, logZ=0.0)
-
-    # Feed pf to the sampler.
+    gflownet = TBGFlowNet(pf=pf_estimator, pb=pb_estimator, logZ=0.0).to(device)
     sampler = Sampler(estimator=pf_estimator)
-    gflownet = gflownet.to(env.device)
 
-    # 3. Create the optimizer.
+    # 3. Create the optimizer and Lr scheduler
     optimizer = torch.optim.Adam(gflownet.pf_pb_parameters(), lr=args.lr)
     optimizer.add_param_group({"params": gflownet.logz_parameters(), "lr": args.lr_logz})
-
-    loss_history = []
-    reward_history = []
-
-    # Learning rate scheduler
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, mode="min", factor=0.5, patience=args.lr_patience
     )
 
+    loss_history = []
+    reward_history = []
+    reward_components_history = []
+
     # 4. Train the gflownet.
     logging.info("Starting training loop...")
 
-    start_time = time.time()
+    for it in tqdm(range(args.n_iterations), dynamic_ncols=True):
 
-    loss_history, reward_history, reward_components, _ = train(
-        args, env, gflownet, sampler, optimizer, scheduler, device
-    )
+        # Sample new reward weights from a Dirichlet distribution
+        weights = (np.random.dirichlet([1, 1, 1])).tolist()
+        env.set_weights(weights)
 
-    total_time = time.time() - start_time
+        # Sample trajectories using the current policy
+        trajectories = sampler.sample_trajectories(
+            env, args.batch_size, save_logprobs=True, epsilon=args.epsilon
+        )
 
-    logging.info(f"Training completed in {total_time:.2f} seconds.")
+        optimizer.zero_grad()
+        loss = gflownet.loss(env, trajectories, recalculate_all_logprobs=False)
+        loss.backward()
+        optimizer.step()
+        scheduler.step(loss)
 
-    plot_training_curves(loss_history, reward_components)
+        # Evaluate final states
+        with torch.no_grad():
+            final_states = trajectories.terminating_states.tensor.to(device)
+            rewards, components = [], []
 
-    start_inference_time = time.time()
+            for state in final_states:
+                state = state.to(device)
+                r, c = compute_reward(state, env.codon_gc_counts, env.weights)
+                rewards.append(r)
+                components.append(c)
 
-    # Sample final sequences
+        avg_reward = torch.mean(torch.tensor(rewards)).item()
+        reward_history.append(avg_reward)
+        reward_components_history.extend(components)
+        loss_history.append(loss.item())
+
+    plot_training_curves(loss_history, reward_components_history)
+
+    # Sample final sequences using fixed reward weights (eg, 30% GC, 30% MFE, 40% CAI) to evaluate policy performance
     with torch.no_grad():
-
         samples, gc_list, mfe_list, cai_list = evaluate(
             env, sampler, weights=torch.tensor([0.3, 0.3, 0.4]), n_samples=args.n_samples
         )
 
-    inference_time = time.time() - start_inference_time
-    avg_time_per_seq = inference_time / args.n_samples
-
-    logging.info("Saving trained model and metrics...")
     torch.save(
         {
             "model_state": gflownet.state_dict(),
@@ -116,7 +126,7 @@ def main(args):
     )
 
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"outputs/generated_sequences_{timestamp}.txt"
+    filename = f"generated_sequences_{timestamp}.txt"
 
     sorted_samples = sorted(samples.items(), key=lambda x: x[1][0], reverse=True)
 
@@ -131,72 +141,47 @@ def main(args):
             )
 
     logging.info(f"Saving generated sequences to {filename}")
-    logging.info(f"Average time per generated sequence {avg_time_per_seq}")
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-
     parser.add_argument("--no_cuda", action="store_true", help="Prevent CUDA usage")
-
     parser.add_argument("--seed", type=int, default=42, help="Random seed")
-
+    parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
     parser.add_argument(
-        "--lr",
-        type=float,
-        default=1e-3,
-        help="Learning rate for the estimators' modules",
+        "--lr_logz", type=float, default=1e-1, help="Learning rate for logZ"
     )
     parser.add_argument(
-        "--lr_logz",
-        type=float,
-        default=1e-1,
-        help="Learning rate for the logZ parameter",
-    )
-
-    parser.add_argument(
-        "--n_iterations", type=int, default=100, help="Number of iterations"
+        "--n_iterations", type=int, default=10, help="Number of training iterations"
     )
     parser.add_argument(
-        "--n_samples", type=int, default=20, help="Number of samples to generate"
-    )
-
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        default=2,
-        help="Batch size, i.e. number of trajectories to sample per training iteration",
+        "--n_samples", type=int, default=20, help="Number of evaluation samples"
     )
     parser.add_argument(
-        "--epsilon", type=float, default=0.1, help="Epsilon for the sampler"
-    )
-
-    parser.add_argument(
-        "--embedding_dim", type=int, default=32, help="Dimension of codon embeddings"
-    )
-
-    parser.add_argument(
-        "--hidden_dim", type=int, default=256, help="Hidden dimension of the networks"
+        "--batch_size", type=int, default=2, help="Batch size for training"
     )
     parser.add_argument(
-        "--n_hidden", type=int, default=2, help="Number of hidden layers"
+        "--epsilon", type=float, default=0.1, help="Epsilon for sampler exploration"
     )
     parser.add_argument(
-        "--tied", action="store_true", help="Whether to tie the parameters of PF and PB"
-    )
-
-    parser.add_argument(
-        "--clip_grad_norm", type=float, default=1.0, help="Gradient clipping norm"
+        "--embedding_dim", type=int, default=32, help="Codon embedding dimension"
     )
     parser.add_argument(
-        "--lr_patience",
-        type=int,
-        default=10,
-        help="Patience for learning rate scheduler",
+        "--hidden_dim", type=int, default=256, help="MLP hidden layer dimension"
     )
-
-    parser.add_argument("--config_path", type=str, default="config.yaml")
+    parser.add_argument(
+        "--n_hidden", type=int, default=2, help="Number of MLP hidden layers"
+    )
+    parser.add_argument(
+        "--tied", action="store_true", help="Tie PF and PB network trunks"
+    )
+    parser.add_argument(
+        "--lr_patience", type=int, default=10, help="LR scheduler patience"
+    )
+    parser.add_argument(
+        "--config_path", type=str, default="config.yaml", help="Path to config YAML"
+    )
 
     args = parser.parse_args()
     config = load_config(args.config_path)
