7ceced43e1793f109de11df9244b87b7452a4e04
ayalaajil <ayalaajil132002@gmail.com>
2025-07-31 14:42:32 +0000

    Add Env desc

--- tutorials/examples/mRNA_design/env.py
+++ tutorials/examples/mRNA_design/env.py
@@ -1,3 +1,16 @@
+r"""Custom mRNA codon design environment using torchgfn to generate mRNA sequences encoding a given protein. It supports a multi-objective optimization over biological properties of mRNA sequences. Implemented using the DiscreteEnv class.
+Each timestep corresponds to choosing a synonymous codon for the next amino acid in the sequence.
+**Action Space**:
+Number of CODONS + 1 possible actions (all codons + 1 exit action)
+**State Representation**:
+A vector of length = protein length, initialized to -1. Codons are filled in step-by-step.
+Masking (Action Constraints)**:
+At each position t, only codons that correspond to the t-th amino acid are allowed to ensure biological correctness.
+**Reward function**:
+A combination of multiple biological properties to evaluate the mRNA sequence. Weights of these objectives can be updated dynamically to reflect different reward configurations. Rewards and constraints are modular and can be extended to incorporate new objectives.
+The environment is customizable for different organisms by using species-specific codon tables preferences, and could serve as a benchmark environment in computational biology. This enables exploration of codon space, which is a large search space given a protein sequence, to optimize for mRNA design.  Applicable to mRNA vaccines, protein therapeutics, and gene expression optimization.
+"""
+
 from typing import Union
 
 import torch
