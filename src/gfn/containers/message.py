d086bee24d96545022df24d29acf64f58bf2e633
Joseph Viviano <joseph@viviano.ca>
2025-09-23 20:18:11 -0400

    Rename Message class attributes for clarity, imported annotations

--- src/gfn/containers/message.py
+++ src/gfn/containers/message.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from enum import Enum, auto
 from typing import Any
 
@@ -11,17 +12,19 @@ class MessageType(Enum):
 
 
 class Message:
-    def __init__(self, type: MessageType, data: Any = None):
-        self.type = type
-        self.data = data
+    def __init__(self, message_type: MessageType, message_data: Any = None):
+        self.message_type = type
+        self.message_data = data
 
     def serialize(self) -> torch.ByteTensor:
         """Convert message into a tensor of bytes."""
         obj_bytes = pickle.dumps(self)
+        
         return torch.ByteTensor(list(obj_bytes))
 
     @staticmethod
     def deserialize(byte_tensor: torch.ByteTensor) -> Message:
         """Reconstruct Message from a tensor of bytes."""
         obj_bytes = bytes(byte_tensor.tolist())
+        
         return pickle.loads(obj_bytes)
