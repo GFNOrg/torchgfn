bugfix for graph sample masking

src/gfn/estimators.py
@@ -740,7 +740,12 @@ class DiscreteGraphPolicyEstimator(LogitBasedEstimator):
         # fallbacks for the unused components.
         no_possible_node_class = torch.isneginf(logits[Ga.NODE_CLASS_KEY]).all(-1)
         no_possible_node_index = torch.isneginf(logits[Ga.NODE_INDEX_KEY]).all(-1)
-        no_possible_add_node = no_possible_node_class | no_possible_node_index
+
+        # If backward, we only need to check if the node index is possible to remove.
+        # If forward, we only need to check if the node class is possible to add.
+        no_possible_add_node = (
+            no_possible_node_index if self.is_backward else no_possible_node_class
+        )
         logits[Ga.ACTION_TYPE_KEY][no_possible_add_node, GaType.ADD_NODE] = -float("inf")
         logits[Ga.NODE_CLASS_KEY][no_possible_node_class] = 0.0
         logits[Ga.NODE_INDEX_KEY][no_possible_node_index] = 0.0